bbb9cac405a254f99abe982ef8fc8bc5959da860
alberth94121 <alberth94121@yahoo.com>
2016-07-25 20:14:30 -0700

    Added database api, cleaned up index,pc,search pages
    Added database api, cleaned up index,pc,search pages

---
+++ api.php
@@ -0,0 +1,20 @@
+<?php
+
+
+function dbConnect(){
+	 $host = "localhost";
+	 $user = "root";
+	 $pw = "";
+	 $db = "test2";
+	 $db_connection = mysql_connect($host, $user,$pw);
+	 mysql_select_db($db, $db_connection);
+	 if(!$db_connection)
+	 {
+	   $errmsg = mysql_error($db_connection);
+	   print "Connection failed: $errmsg <br />";
+	   exit(1);
+	 }
+}
+
+
+?>
--- search.php
+++ search.php
@@ -1,12 +1,8 @@
 <!DOCTYPE HTML>
 
 <?php
-	$dbhost = 'localhost';
-	$dbuser = 'root';
-	$dbpass = '';
-	$db = 'test2';
-	$conn = mysql_connect($dbhost, $dbuser, $dbpass)or die("Can't connect");;
-	mysql_select_db($db, $conn);
+	include 'api.php';
+	dbConnect();
 	
 	if(isset($_GET["page"])){ 
 		$page  = $_GET["page"]; 
@@ -20,19 +16,20 @@
 	else{ 
 		$search=""; 
 	};
+	if(isset($_GET["id"])){ 
+		$system = $_GET["id"]; 
+	} 
+	else{ 
+		$system=""; 
+	};
 	$perPage = 2;
 	$start = ($page-1) * $perPage; 
-	$sql = "SELECT COUNT(gameID) FROM pcGames WHERE name LIKE '%$search%'"; 
+	$sql = "SELECT COUNT(gameID) FROM Games WHERE (systemID=$system AND name LIKE '%$search%')"; 
 	$rs = @mysql_query($sql); 
 	$row = mysql_fetch_row($rs);  
 	$total = ceil($row[0] / $perPage); 
-
-
-
-
-
-
 ?> 
+
 <html>
 	<head>
 			<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
@@ -69,57 +66,67 @@
 	</head>
 
 	<body>
-		  <!--HEADER ROW-->
-		  <div id="header-row">
+		<!-- Header -->
+		<div id="header-row">
 			<div class="container">
-			  <div class="row">
-					  <!--LOGO-->
-					  <div class="span3"><h1>NextGame</h1></div>
-					  <!-- /LOGO -->
+				<div class="row">
+					<!-- Logo -->
+					<div class="span3"><img src="img/NextGame.png"></div>
 
-					<!-- MAIN NAVIGATION -->  
-					  <div class="span9">
+					<!-- Nav -->  
+					<div class="span9">
 						<div class="navbar  pull-right">
-						  <div class="navbar-inner">
+							<div class="navbar-inner">
 								<a data-target=".navbar-responsive-collapse" data-toggle="collapse" class="btn btn-navbar"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a>
 								<div class="nav-collapse collapse navbar-responsive-collapse">
 									<ul class="nav">
 										<li class="active"><a href="index.html">Home</a></li>
-										<li class="active"><a href="test.php">PC</a></li>
-										<li class="active"><a href="index.html">Xbox</a></li>
+										<li class="active"><a href="pc.php">PC</a></li>
 										<li class="active"><a href="index.html">PS4</a></li>
+										<li class="active"><a href="index.html">Xbox</a></li>
+										<li class="active"><a href="index.html">3DS</a></li>
 									</ul>
 								</div>
-						   </div>
+							</div>
 						</div>
-					  </div>
-			  </div>
-			</div>
-		  </div>
-		  
-		<div class="container">		
-				<!--Home-->
-				<div class="row">
-					  <div class="span12 cnt-title2" style="padding:20px">
-					   <h1 style="text-align:left;margin:20px">PC Games</h1>  
-					  </div>				   
+					</div>
 				</div>
+			</div>
+		</div>
+		  
+		<!-- Title -->
+		<div class="container">		
+			<div class="row">
+					  <div class="span12 cnt-title2" style="padding:20px">
+					  <?php
+					     if($system == 1){
+							echo '<h1 style="text-align:left;margin:20px">PC Games</h1>'; 
+						 }
+						 else
+							 echo '<h1 style="text-align:left;margin:20px">All Games</h1>';
+					  ?>
+					  </div>				   
+			</div>
 		</div>
 		
+		<!-- SearchBos -->
 		<div class="container">	
 			<div class="row">	
 				<div class="span12 cnt-title2" >
 					<form action="search.php?" >
-							<input type="text" name="query" style="height: 15px;"/><br> 
-							<input type="hidden" name="page" value="1" />							
-							<input  class='btn btn-default' type="submit" value="Search"> 
+						<?php
+							$text2='<input type="hidden" name="id" value="'.$system.'" />';
+							echo $text2;				
+						?>	
+						<input type="text" name="query" style="height: 15px;"/><br> 
+						<input type="hidden" name="page" value="1" />	
+						<input  class='btn btn-default' type="submit" value="Search"> 
 					</form>	
 				</div>
 			</div>
 		</div>
 
-
-
+		<!-- Pages -->
 		<div class="container">	
 			<div class="row">	
 				<div class="span12 cnt-title2" style="padding:0px">
@@ -127,20 +134,20 @@
 					<?php
 						if($page > 1){
 							$p=$page-1;
-							echo "<a  href='search.php?page=$p&query=$search' class='btn btn-default'>Prev </a>";
+							echo "<a  href='search.php?id=$system&query=$search&page=$p' class='btn btn-default'>Prev </a>";
 							echo ' ';
 						}
-						else {echo "<a class='btn btn-default'>Prev</a>";
-						echo ' ';}
+						else {
+							echo "<a class='btn btn-default'>Prev</a>";
+							echo ' ';
+						}
 						
 						$pre = $page;
 						$amt = 5;
 						if($pre >= 1+ $amt){
 							$num = $pre-$amt;
 							while($num < $pre){
-								
-							
-								echo "<a href='search.php?page=$num&query=$search' class='btn btn-default'>$num </a>";
+								echo "<a href='search.php?id=$system&query=$search&page=$num' class='btn btn-default'>$num </a>";
 								echo ' ';
 								$num+=1;
 							}
@@ -148,13 +155,13 @@
 						else{
 							$num = 1;
 							while($num < $pre){
-								
-								echo "<a href='search.php?page=$num&query=$search' class='btn btn-default' >$num </a>";
+								echo "<a href='search.php?id=$system&query=$search&page=$num' class='btn btn-default' >$num </a>";
 								echo ' ';
 								$num+=1;
 							}
 						
 						}
+						
 						echo "<a class='btn btn-default' ><strong>$page</strong></a>";
 						echo ' ';
 						
@@ -162,8 +169,7 @@
 						if($pre <= $total-$amt){
 							$num = $pre+1;
 							while($num <= $pre+$amt){
-								
-								echo "<a class='btn btn-default' href='search.php?page=$num&query=$search'>$num </a>";
+								echo "<a class='btn btn-default' href='search.php?id=$system&query=$search&page=$num'>$num </a>";
 								echo ' ';
 								$num+=1;
 							}
@@ -171,64 +177,51 @@
 						else{
 							$num = $pre+1;
 							while($num <= $total){
-								
-								echo "<a class='btn btn-default' href='search.php?page=$num&query=$search'>$num </a>";
+								echo "<a class='btn btn-default' href='search.php?id=$system&query=$search&page=$num'>$num </a>";
 								echo ' ';
 								$num+=1;
 							}
-						
 						}
 						
-
-						
 						if($page < $total){
-							$p=$page+1;
-							echo "<a class='btn btn-default' href='search.php?page=$p&query=$search'> Next</a>";
+							$next=$page+1;
+							echo "<a class='btn btn-default' href='search.php?id=$system&query=$search&page=$next'> Next</a>";
 							echo ' ';
 								
 						}
-						else {echo "<a class='btn btn-default'>Next</a>";
-							echo ' ';}
-						
-					
+						else {
+							echo "<a class='btn btn-default'>Next</a>";
+							echo ' ';
+						}
 					?>
 				</p>
 				</div>
-					
-
 			</div>
 		</div>
 
-	
- 
+		<!-- Table -->
 		<div class="container">	
 			<div class="span12" >
-		
-
-				<table >
+				<table>
 					<thead>
-					  <tr>
-						<th>
-						  
-						</th>
-						<th>
-						  Name
-						</th>
-						<th>
-						  Description
-						</th>
-						
-						<th class="last">
-						  Release Date
-						</th>
-
-						
-					  </tr>
+						<tr>
+							<th>
+							</th>
+							<th>
+							  Name
+							</th>
+							<th>
+							  Description
+							</th>
+							<th class="last">
+							  Release Date
+							</th>
+							</tr>
 					</thead>
 					
 					<tbody>
 					<?php
-						$query = @mysql_query("SELECT * FROM pcGames WHERE name LIKE '%$search%' ORDER BY releaseDate LIMIT $start,$perPage");
+						$query = @mysql_query("SELECT * FROM Games WHERE (systemID=$system AND name LIKE '%$search%') ORDER BY releaseDate LIMIT $start,$perPage");
 						
 						while( $row = @mysql_fetch_assoc($query)) 
 						{
@@ -237,7 +230,6 @@
 							$name = $row['name'];
 							$description = $row['description'];
 							$date = $row['releaseDate'];
-							$link = $row['link'];
 							
 							echo "<tr><td>$src</td><td>$name</td><td>$description</td><td>$date</td>";
 						 
@@ -245,7 +237,6 @@
 					?>
 					  
 					</tbody>
-	   
 				</table>
 					
 			</div>
